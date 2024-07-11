fc46d84f19e842df9af6b6d18820c0571c2e80aa
yasin_efendi_cetin <yasinefendicetin@gmail.com>
2024-07-11 16:55:50 +0300

    add time label

--- functions.py
+++ functions.py
@@ -1,11 +1,16 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH="todos_item.txt"
+def get_todos(filepath=FILEPATH):
     """Read the text file and return the list of to-do items"""
     with open(filepath,"r") as file_local:
         todos_local=file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg,filepath="todos.txt"):
+def write_todos(todos_arg,filepath=FILEPATH):
     """Write the to-do items list in text file """
     with open(filepath,"w") as file:
         file.writelines(todos_arg)
+
+if __name__== "__main__"
+    print("Hello")
+    print(get_todos())
